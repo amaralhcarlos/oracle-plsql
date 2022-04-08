ba7e2868663ff83bb248b3d9a9b9ed3f92917b2a
Carlos Henrique do Amaral <amaralhcarlos@gmail.com>
2022-04-08 10:35:32 -0300

    Create create_table_cliente.sql

---
+++ create_table_cliente.sql
@@ -0,0 +1,19 @@
+CREATE TABLE
+    cliente(
+        id number(5),
+        nome VARCHAR2(100),
+        segmercado_id number(5),
+        data_inclusao DATE,
+        faturamento_previsto number(10, 2),
+        categoria VARCHAR2(20)
+    );
+
+ALTER TABLE
+    cliente
+ADD
+    CONSTRAINT pk_cliente_id PRIMARY key(id);
+
+ALTER TABLE
+    cliente
+ADD
+    CONSTRAINT fk_segmercado_id FOREIGN key(segmercado_id) REFERENCES segmercado(id);
