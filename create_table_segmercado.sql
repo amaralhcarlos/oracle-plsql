5d2353646d4c21076f3b1a8b9383c01365a6f88b
Carlos Henrique do Amaral <amaralhcarlos@gmail.com>
2022-04-08 10:30:24 -0300

    Create create_table_segmercado.sql

---
+++ create_table_segmercado.sql
@@ -0,0 +1,15 @@
+CREATE TABLE
+    segmercado(
+        id number(5),
+        descricao VARCHAR2(100),
+        PRIMARY key(id)
+    );
+
+--Criando a chave primária manualmente sem usar o comando PRIMARY KEY diretamente na tabela.
+ALTER TABLE
+    segmercado
+ADD
+    CONSTRAINT pk_segmercado_id PRIMARY key(id);
+
+DROP TABLE
+    segmercado;
