863dc2810ae4b1ffc15f08928f565032f12276ea
Carlos Henrique do Amaral <amaralhcarlos@gmail.com>
2022-04-11 13:50:49 -0300

    Popula segmercado

--- insert_into_segmercado.sql
+++ insert_into_segmercado.sql
@@ -1,14 +1,14 @@
 INSERT INTO segmercado (id, descricao)
-VALUES (1, "Varejista");
+VALUES (1, 'Varejista');
 
 INSERT INTO segmercado(id, descricao)
-VALUES (2, "Atacadista");
+VALUES (2, 'Atacadista');
 
 INSERT INTO segmercado(id, descricao)
-VALUES (3, "Farmaceutico");
+VALUES (3, 'Farmaceutico');
 
 INSERT INTO segmercado(id, descricao)
-VALUES (4, "Industrial");
+VALUES (4, 'Industrial');
 
 INSERT INTO segmercado(id, descricao)
-VALUES (5, "Agropecuária");
+VALUES (5, 'Agropecuária');
