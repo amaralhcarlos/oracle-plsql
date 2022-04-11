7f2e4df0d082f2180743de9f028889c35ba6cb90
Carlos Henrique do Amaral <amaralhcarlos@gmail.com>
2022-04-11 13:48:04 -0300

    Create insert_into_clientes.sql
    Popula tabela cliente

---
+++ insert_into_clientes.sql
@@ -0,0 +1,79 @@
+INSERT INTO
+    cliente(
+        id,
+        nome,
+        cnpj,
+        segmercado_id,
+        data_inclusao,
+        faturamento_previsto,
+        categoria
+    )
+VALUES (
+        1,
+        'SUPERMERCADO XYZ',
+        '12/345',
+        5,
+        sysdate,
+        150000,
+        'GRANDE'
+    );
+
+INSERT INTO
+    cliente(
+        id,
+        nome,
+        cnpj,
+        segmercado_id,
+        data_inclusao,
+        faturamento_previsto,
+        categoria
+    )
+VALUES (
+        2,
+        'SUPERMERCADO IJK',
+        '67/890',
+        1,
+        sysdate,
+        90000,
+        'MEDIO GRANDE'
+    );
+
+INSERT INTO
+    cliente(
+        id,
+        nome,
+        cnpj,
+        segmercado_id,
+        data_inclusao,
+        faturamento_previsto,
+        categoria
+    )
+VALUES (
+        3,
+        'SUPERMERCADO IJK',
+        '89/012',
+        3,
+        sysdate,
+        80000,
+        'MEDIO GRANDE'
+    );
+
+INSERT INTO
+    cliente (
+        id,
+        nome,
+        cnpj,
+        segmercado_id,
+        data_inclusao,
+        faturamento_previsto,
+        categoria
+    )
+VALUES (
+        4,
+        'FARMACIA AXZ',
+        '12/378',
+        3,
+        sysdate,
+        80000,
+        'MEDIO GRANDE'
+    );
